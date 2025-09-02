33f9243fc082d39b07e92d9c523f5c359bc59c80
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 17:29:44 +0530

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Example2 {
+		static String HelloWorld() {
+			System.out.println("HelloWorld");
+			return "";
+		}
+	public static void main(String[] args) {
+		HelloWorld();
+		HelloWorld();
+		HelloWorld();
+
+	}
+
+}
