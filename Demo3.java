102f27d6607fc2d587f4a04b9454bb0ce7cc35fc
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 16:34:51 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int i=99;
+		double d=137.89;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+		System.out.println("After type conversion:");
+		i=(int)d;
+		System.out.println("i="+i);
+		System.out.println("d="+d);
+
+	}
+
+}
