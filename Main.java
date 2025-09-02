4dd0905df3fb1bb953d1345ee51847e2f7c7d61c
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 17:25:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("java");
+		System.out.println("SQL");
+		System.out.println("python");
+
+	}
+
+}
