96dacabd18d4902dab8b3de5e0603a3bfe3d27c3
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 17:30:25 +0530

    Add files via upload

---
+++ Method2.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Method2 {
+		static void Method(int val)
+		{
+			System.out.println("parameter and no return value method");
+		}
+	public static void main(String[] args) {
+			Method(10);
+
+	}
+
+}
