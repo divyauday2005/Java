07f9bed29fd59bcb09765c19f94b7f747cb348ec
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 16:35:12 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Demo2 {
+
+public static void main(String[] args ) {
+			int i= 99;
+			double d=135.99;
+			System.out.println("i="+i);
+			System.out.println("b="+d);
+			System.out.println("After primitive type conversion");
+			d=i;
+			System.out.println("i="+i);
+			System.out.println("b="+d);
+     }
+}
