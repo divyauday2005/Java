10965109ba5ecec32e216ff6f7b13ab5a5eb75d0
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 17:25:12 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Demo1 {
+		static void Display() {
+			System.out.println("java");
+			System.out.println("SQL");
+			System.out.println("python");
+		}
+		public static void main() {
+			Display();
+			Display();
+		}
+}
