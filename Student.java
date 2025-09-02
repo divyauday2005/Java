0f9c568b51623aaefb426d098947f2b3a549593f
divyauday2005 <divyauday2005@gmail.com>
2025-09-02 16:27:08 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,57 @@
+package day2;
+
+public class Student {
+	String name;
+	int regnum;
+	String branch;
+	double percentage;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Student S1=new Student();
+			S1.name="Nancy";
+			S1.regnum=1016;
+			S1.branch="CSD";
+			S1.percentage=87.9;
+		    System.out.println("Student name :"+S1.name);
+		    System.out.println("Register number :"+S1.regnum);
+		    System.out.println("Branch :"+S1.branch);
+		    System.out.println("Percentage :"+S1.percentage);
+		    Student S2=new Student();
+		    S2.name="Millie";
+			S2.regnum=1011;
+			S2.branch="CSm";
+			S2.percentage=80.9;
+			System.out.println("Student name :"+S2.name);
+			System.out.println("Register number :"+S2.regnum);
+			System.out.println("Branch :"+S2.branch);
+			System.out.println("Percentage :"+S2.percentage);
+			Student S3=new Student();
+			S3.name="Steve";
+			S3.regnum=1017;
+			S3.branch="EEE";
+			S3.percentage=77.9;
+			System.out.println("Student name :"+S3.name);
+			System.out.println("Register number :"+S3.regnum);
+			System.out.println("Branch :"+S3.branch);
+			System.out.println("Percentage :"+S3.percentage);
+			Student S4 = new Student();
+			S4.name="Cassie";
+			S4.regnum=1011;
+			S4.branch="ECE";
+			S4.percentage=70.9;
+			System.out.println("Student name :"+S4.name);
+			System.out.println("Register number :"+S4.regnum);
+			System.out.println("Branch :"+S4.branch);
+			System.out.println("Percentage :"+S4.percentage);
+			Student S5=new Student();
+			S5.name="Nate";
+			S5.regnum=1001;
+			S5.branch="CSB";
+			S5.percentage=70.9;
+			System.out.println("Student name :"+S5.name);
+			System.out.println("Register number :"+S5.regnum);
+			System.out.println("Branch :"+S5.branch);
+			System.out.println("Percentage :"+S5.percentage);
+	}
+
+}
